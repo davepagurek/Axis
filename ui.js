74d10b0778325cff4d0c68a3a6a16f8e0f0080d9
Abhishek Madan <abhi.madan01@gmail.com>
2015-03-08 03:11:39 -0400

    add functionality to create person button

--- ui.js
+++ ui.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-    $("createKeyFrame").click(function(){
+    $("#createKeyFrame").click(function(){
         newFrame = document.getElementById("Frame").value;
-        population.forEach(function(element) {
+        pop.population.forEach(function(element) {
             axis.clear(element);
             axis.create(element, newFrame);
             axis.frame = newFrame;
         });
         console.log(newFrame);
-        population.forEach(function(element) {
-            axis.createNewFrame(element, curFrame, newFrame);
+        pop.population.forEach(function(element) {
+            axis.createNewFrame(element, axis.frame, newFrame);
         });
     });
 
     $("#setFrame").click(function(){
         curFrame = document.getElementById("Frame").value;
-        population.forEach(function(element) {
+        pop.population.forEach(function(element) {
             console.log(axis.getLocation(element.frames, curFrame));
         });
     });
@@ -30,5 +30,12 @@ $(document).ready(function() {
     $("#createFrame").click(function(){
         $('#frame_list tr').append('<td><div class="frame"></div></td>');
         $('#frame_list tr td:last .frame').click(frameClick);
+    });
+
+    $("#createPerson").click(function(){
+        var newStickman = pop.addStickman();
+        pop.toPaper(newStickman);
+        pop.population.push(newStickman);
+        axis.create(newStickman, axis.frame);
     });
 });

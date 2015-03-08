a7ff429bdc5b95372b02b94d263c75c9ac4625f1
Abhishek Madan <abhi.madan01@gmail.com>
2015-03-08 10:55:09 -0400

    add save button and functionality

--- ui.js
+++ ui.js
@@ -83,6 +83,10 @@ $(document).ready(function() {
         }
     });
 
+    $("#save").click(function(){
+        console.log(pop.save());
+    });
+
     $(window).keydown(function(event){
         var NEXT_FRAME = 190;
         var PREV_FRAME = 188;
